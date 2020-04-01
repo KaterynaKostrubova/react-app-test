6b43e6d8de672be266f0819093f1292cc646c64f
KaterynaKostrubova <katerinakostrubova@gmail.com>
2020-04-01 19:27:31 +0300

    Updates

---
+++ precache-manifest.7232e6e7994ff1d4219acc0ca03aeba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021c471e5b46ff631dc35241f5034ef8",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "567f7404e37b56290dc5",
+    "url": "/react-app-test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "81a540b57a576948e91b",
+    "url": "/react-app-test/static/css/main.ff79dffa.chunk.css"
+  },
+  {
+    "revision": "567f7404e37b56290dc5",
+    "url": "/react-app-test/static/js/2.1628c97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-test/static/js/2.1628c97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a540b57a576948e91b",
+    "url": "/react-app-test/static/js/main.fc31d0dc.chunk.js"
+  },
+  {
+    "revision": "fbc3de2d90c9c3baeeec",
+    "url": "/react-app-test/static/js/runtime-main.b1a5e90b.js"
+  }
+]);
