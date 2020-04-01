56d6170de998757021f3f5f793534183bdc147da
KaterynaKostrubova <katerinakostrubova@gmail.com>
2020-04-01 19:47:13 +0300

    test

--- src/components/MoviesPagination.jsx
+++ src/components/MoviesPagination.jsx
@@ -13,7 +13,6 @@ class MoviesPagination extends React.Component {
             }
         };
 
-
         const handlePrev = (value) => {
             if(value > 1){
                 return(event) => {
@@ -32,20 +31,16 @@ class MoviesPagination extends React.Component {
         return(
             <div className="d-flex justify-content-center">
                 <button type="button" id='prev' className='btn btn-sm btn-dark' onClick={handlePrev(page)}>
-                    PREV
-                </button>
+                    PREV</button>
                 <button type="button" id='first' className='btn text-muted' onClick={handleStatic(1)}>
-                    1
-                </button>
+                    1</button>
                 <div className="btn btn-outline-info">
                     -{page}-
                 </div>
                 <button type="button" id='last' className='btn text-muted' onClick={handleStatic(total_pages)}>
-                    {total_pages}
-                </button>
+                    {total_pages}</button>
                 <button type="button" id='next' className='btn btn-sm btn-dark' onClick={handleNext(page)}>
-                    NEXT
-                </button>
+                    NEXT</button>
             </div>
         )
     }
